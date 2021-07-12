398d2256840d249bd7c0590168a78404404078f8
Ren Ivey <haleyrivey@gmail.com>
2021-07-12 12:21:37 -0400

    Added sticky nav, updated images, added logo.

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ $(".gallery ul li a").click(function() {
         scrollTop: parseInt($("#top").offset().top)
     }, 400);
 });
+
+// sticky nav
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("stickynav");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if ($(window).scrollTop() >= 900) {
+    navbar.classList.add("sticky")
+    $('#stickynav').fadeIn();
+    $('#logo').fadeIn();
+  } else {
+    navbar.classList.remove("sticky");
+    $('#logo').fadeOut();
+  }
+} 
