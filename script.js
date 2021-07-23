078e592309e4f91434543c70964853cb03830d1d
Ren Ivey <haleyrivey@gmail.com>
2021-07-23 13:29:21 -0400

    Removed prev/next/exit buttons from art gallery overlay.

--- script.js
+++ script.js
@@ -43,12 +43,9 @@ $( ".img-wrapper" ).hover(
 // Lightbox
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
-var $prevButton = $('<div id="prevButton"><i class="fa fa-chevron-left"></i></div>');
-var $nextButton = $('<div id="nextButton"><i class="fa fa-chevron-right"></i></div>');
-var $exitButton = $('<div id="exitButton"><i class="fa fa-times"></i></div>');
 
 // Add overlay
-$overlay.append($image).prepend($prevButton).append($nextButton).append($exitButton);
+$overlay.append($image);
 $(".art-gallery").append($overlay);
 
 // Hide overlay on default
@@ -70,50 +67,4 @@ $(".img-overlay").click(function(event) {
 $overlay.click(function() {
   // Fade out the overlay
   $(this).fadeOut("slow");
-});
-
-// When next button is clicked
-$nextButton.click(function(event) {
-  // Hide the current image
-  $("#overlay img").hide();
-  // Overlay image location
-  var $currentImgSrc = $("#overlay img").attr("src");
-  // Image with matching location of the overlay image
-  var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
-  // Finds the next image
-  var $nextImg = $($currentImg.closest(".image").next().find("img"));
-  // All of the images in the gallery
-  var $images = $("#image-gallery img");
-  // If there is a next image
-  if ($nextImg.length > 0) { 
-    // Fade in the next image
-    $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
-  } else {
-    // Otherwise fade in the first image
-    $("#overlay img").attr("src", $($images[0]).attr("src")).fadeIn(800);
-  }
-  // Prevents overlay from being hidden
-  event.stopPropagation();
-});
-
-// When previous button is clicked
-$prevButton.click(function(event) {
-  // Hide the current image
-  $("#overlay img").hide();
-  // Overlay image location
-  var $currentImgSrc = $("#overlay img").attr("src");
-  // Image with matching location of the overlay image
-  var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
-  // Finds the next image
-  var $nextImg = $($currentImg.closest(".image").prev().find("img"));
-  // Fade in the next image
-  $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
-  // Prevents overlay from being hidden
-  event.stopPropagation();
-});
-
-// When the exit button is clicked
-$exitButton.click(function() {
-  // Fade out the overlay
-  $("#overlay").fadeOut("slow");
 });
