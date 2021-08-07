49318ca4469e88517942eb9c8f6a69f4b29680f4
Ren Ivey <haleyrivey@gmail.com>
2021-08-07 18:09:29 -0400

    Switching over to bootstrap.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Lightbox
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  // event.preventDefault();
+  $(this).ekkoLightbox();
+}); 
+ 
  // Sticky nav settings.
 window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("stickynav");
@@ -12,59 +18,3 @@ function myFunction() {
     $('#logo').fadeOut();
   }
 } 
- 
- // Web Development examples.
- $('.web-gallery ul li a').click(function() {
-    var itemID = $(this).attr('href');
-    $('.web-gallery ul').addClass('item_open');
-    $(itemID).addClass('item_open');
-    return false;
-});
-$('.close').click(function() {
-    $('.port, .web-gallery ul').removeClass('item_open');
-    return false;
-});
-
-$(".web-gallery ul li a").click(function() {
-    $('html, body').animate({
-        scrollTop: parseInt($("#top").offset().top)
-    }, 400);
-});
-
-// Digital art gallery settings.
-$( ".img-wrapper" ).hover(
-  function() {
-    $(this).find(".img-overlay").animate({opacity: 1}, 600);
-  }, function() {
-    $(this).find(".img-overlay").animate({opacity: 0}, 600);
-  }
-);
-
-// Lightbox
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-
-// Add overlay
-$overlay.append($image);
-$(".art-gallery").append($overlay);
-
-// Hide overlay on default
-$overlay.hide();
-
-// When an image is clicked
-$(".img-overlay").click(function(event) {
-  // Prevents default behavior
-  event.preventDefault();
-  // Adds href attribute to variable
-  var imageLocation = $(this).prev().attr("href");
-  // Add the image src to $image
-  $image.attr("src", imageLocation);
-  // Fade in the overlay
-  $overlay.fadeIn("slow");
-});
-
-// When the overlay is clicked
-$overlay.click(function() {
-  // Fade out the overlay
-  $(this).fadeOut("slow");
-});
